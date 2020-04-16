7e2868f1849544313c7afa78d28aa517af9a9462
bigrocs <bigrocs@qq.com>
2020-04-16 18:23:31 +0800

    自动提交 git 代码

--- config/config.go
+++ config/config.go
@@ -22,20 +22,20 @@ var Conf config.Config = config.Config{
 	},
 	Permissions: []*PB.Permission{
 		// 支付管理
-		{Method: "Pays.AopF2F", Auth: false, Policy: false, Name: "商家扫用户", Description: "商家扫用户付款码付款。"},
+		{Service: "pay-api", Method: "Pays.AopF2F", Auth: false, Policy: false, Name: "商家扫用户", Description: "商家扫用户付款码付款。"},
 		// 订单管理
-		{Method: "Orders.Get", Auth: true, Policy: true, Name: "查询订单", Description: "查询订单信息权限。"},
-		{Method: "Orders.Update", Auth: true, Policy: true, Name: "更新订单", Description: "更新订单信息。"},
-		{Method: "Orders.List", Auth: true, Policy: true, Name: "订单列表", Description: "查询订单列表"},
-		{Method: "Orders.SelfList", Auth: true, Policy: false, Name: "本账户订单列表", Description: "本账户订单列表。"},
-		{Method: "Orders.SelfAmount", Auth: true, Policy: false, Name: "本账户订单统计", Description: "本账户订单统计。"},
+		{Service: "pay-api", Method: "Orders.Get", Auth: true, Policy: true, Name: "查询订单", Description: "查询订单信息权限。"},
+		{Service: "pay-api", Method: "Orders.Update", Auth: true, Policy: true, Name: "更新订单", Description: "更新订单信息。"},
+		{Service: "pay-api", Method: "Orders.List", Auth: true, Policy: true, Name: "订单列表", Description: "查询订单列表"},
+		{Service: "pay-api", Method: "Orders.SelfList", Auth: true, Policy: false, Name: "本账户订单列表", Description: "本账户订单列表。"},
+		{Service: "pay-api", Method: "Orders.SelfAmount", Auth: true, Policy: false, Name: "本账户订单统计", Description: "本账户订单统计。"},
 		// 配置管理
-		{Method: "Configs.SelfUpdate", Auth: true, Policy: false, Name: "更新登陆用户支付配置", Description: "更新登陆用户支付配置信息"},
-		{Method: "Configs.Info", Auth: true, Policy: false, Name: "获取登陆用户支付配置", Description: "获取登陆用户支付配置信息"},
-		{Method: "Configs.Create", Auth: true, Policy: true, Name: "创建支付配置", Description: "创建新角色权限。"},
-		{Method: "Configs.Delete", Auth: true, Policy: true, Name: "删除支付配置", Description: "删除支付配置。"},
-		{Method: "Configs.Update", Auth: true, Policy: true, Name: "更新支付配置", Description: "更新支付配置信息。"},
-		{Method: "Configs.Get", Auth: true, Policy: true, Name: "查询支付配置", Description: "查询支付配置信息权限。"},
-		{Method: "Configs.List", Auth: true, Policy: true, Name: "支付配置列表", Description: "查询支付配置列表"},
+		{Service: "pay-api", Method: "Configs.SelfUpdate", Auth: true, Policy: false, Name: "更新登陆用户支付配置", Description: "更新登陆用户支付配置信息"},
+		{Service: "pay-api", Method: "Configs.Info", Auth: true, Policy: false, Name: "获取登陆用户支付配置", Description: "获取登陆用户支付配置信息"},
+		{Service: "pay-api", Method: "Configs.Create", Auth: true, Policy: true, Name: "创建支付配置", Description: "创建新角色权限。"},
+		{Service: "pay-api", Method: "Configs.Delete", Auth: true, Policy: true, Name: "删除支付配置", Description: "删除支付配置。"},
+		{Service: "pay-api", Method: "Configs.Update", Auth: true, Policy: true, Name: "更新支付配置", Description: "更新支付配置信息。"},
+		{Service: "pay-api", Method: "Configs.Get", Auth: true, Policy: true, Name: "查询支付配置", Description: "查询支付配置信息权限。"},
+		{Service: "pay-api", Method: "Configs.List", Auth: true, Policy: true, Name: "支付配置列表", Description: "查询支付配置列表"},
 	},
 }
